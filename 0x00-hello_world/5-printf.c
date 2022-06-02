2f21e95187af5dedab1c263fac3083bb170d430a
timmypr <timmydihno@gmail.com>
2022-06-02 11:43:41 -0700

    Complicated commit 2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
 /**
-*
-* main - beg
-*
-* Return: stop
-*/
+ *
+ * main - beg
+ *
+ * Return: stop
+ */
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
