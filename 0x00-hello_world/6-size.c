017b732303169ac323e996031f743dc313849613
timmypr <timmydihno@gmail.com>
2022-06-02 13:17:39 -0700

    Task 2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-
 /*
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
-{ 
-	char acharacter; 
-	int ainteger; 
-	long along; 
-	long long alonglong; 
-	float afloat; 
-
+{
+	char acharacter;
+	int ainteger;
+	long along;
+	long long alonglong;
+	float afloat;
 	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter)); 
 	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger)); 
 	printf("Size of a long int: %lu byte(s)\n", sizeof(along)); 
